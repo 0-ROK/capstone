9f48620ad44616cd4807eab58ab9390d92974cb2
조영록 <54888076+0-ROK@users.noreply.github.com>
2022-10-23 11:31:48 +0900

    게시판 작성 글 렌덜이

--- src/pages/study/StudyCommunity.jsx
+++ src/pages/study/StudyCommunity.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "@toast-ui/editor/dist/toastui-editor.css";
 
 import { Editor } from "@toast-ui/react-editor";
-import { Form, Modal } from "react-bootstrap";
+import { Card, Form, Modal } from "react-bootstrap";
 import Button from "@restart/ui/esm/Button";
 import { useNavigate } from "react-router";
+import CardHeader from "react-bootstrap/esm/CardHeader";
 
 const StudyCommunity = () => {
   const navigate = useNavigate();
+
+  const loadedData = useState([]);
 
   return (
     <div>
@@ -23,6 +26,17 @@ const StudyCommunity = () => {
         </Form.Group>
         <Editor />
       </Form>
+
+      {loadedData?.map((ele) => (
+        <Card style={{ width: "18rem", margin: "10px 10px" }}>
+          <Card.Img
+            variant="top"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1ffYg4ePSTDEEc_dNcQfaZvnEg6pFS2WCEQ&usqp=CAU"
+          />
+          <CardHeader title={ele?.title} description={ele?.description} />
+          <div dangerouslySetInnerHTML={{ __html: loadedData?.content }} />
+        </Card>
+      ))}
     </div>
   );
 };
